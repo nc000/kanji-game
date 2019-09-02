47459e81b7296cca55f4fd578db377da8af9307b
nc000 <codeonlync@gmail.com>
2019-09-02 20:43:41 +0100

    Updates

---
+++ precache-manifest.41b945e8eb66f828625896718862ff98.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8503bc5ba402bb0c6a055b762c80f2d",
+    "url": "/kanji-game/index.html"
+  },
+  {
+    "revision": "1b82db69055b9c95d13e",
+    "url": "/kanji-game/static/css/2.b693fe29.chunk.css"
+  },
+  {
+    "revision": "f1b5b0008aad3a632595",
+    "url": "/kanji-game/static/css/main.fe0cc50c.chunk.css"
+  },
+  {
+    "revision": "1b82db69055b9c95d13e",
+    "url": "/kanji-game/static/js/2.66e7c6f9.chunk.js"
+  },
+  {
+    "revision": "f1b5b0008aad3a632595",
+    "url": "/kanji-game/static/js/main.d67a4ee9.chunk.js"
+  },
+  {
+    "revision": "f247a102ea765d49cd15",
+    "url": "/kanji-game/static/js/runtime~main.17de345d.js"
+  },
+  {
+    "revision": "d5eb31aeb62d803110ca73f06c703739",
+    "url": "/kanji-game/static/media/correct.d5eb31ae.wav"
+  },
+  {
+    "revision": "3165b14bbf3b64fca65829ecde6b800d",
+    "url": "/kanji-game/static/media/fa-brands-400.3165b14b.eot"
+  },
+  {
+    "revision": "457cb96b6191ed105c4fe8463957f70d",
+    "url": "/kanji-game/static/media/fa-brands-400.457cb96b.woff"
+  },
+  {
+    "revision": "f249e44df3044e7b0d665b550569ddf0",
+    "url": "/kanji-game/static/media/fa-brands-400.f249e44d.svg"
+  },
+  {
+    "revision": "f2e186cfab4787d4ef6f1bb192aa9a1b",
+    "url": "/kanji-game/static/media/fa-brands-400.f2e186cf.ttf"
+  },
+  {
+    "revision": "f861a57c52ef711cf807a3eec92c0e17",
+    "url": "/kanji-game/static/media/fa-brands-400.f861a57c.woff2"
+  },
+  {
+    "revision": "2cd8d991e82712f1a5c5de8ee869ca74",
+    "url": "/kanji-game/static/media/fa-regular-400.2cd8d991.ttf"
+  },
+  {
+    "revision": "2fad4ee0a47f41bc76e21c2baf613182",
+    "url": "/kanji-game/static/media/fa-regular-400.2fad4ee0.svg"
+  },
+  {
+    "revision": "7ab2cb050690f93994c46258d49af5b6",
+    "url": "/kanji-game/static/media/fa-regular-400.7ab2cb05.woff"
+  },
+  {
+    "revision": "a03df7ab0ffc96cabbfe3dc71c60baaa",
+    "url": "/kanji-game/static/media/fa-regular-400.a03df7ab.eot"
+  },
+  {
+    "revision": "bd52a727b5449dc3f8195b72c9c58341",
+    "url": "/kanji-game/static/media/fa-regular-400.bd52a727.woff2"
+  },
+  {
+    "revision": "2cd2be177470d5096992572176bbe76e",
+    "url": "/kanji-game/static/media/fa-solid-900.2cd2be17.woff2"
+  },
+  {
+    "revision": "5ee32f5c8598e1a63ddf1aad4ffe54f4",
+    "url": "/kanji-game/static/media/fa-solid-900.5ee32f5c.ttf"
+  },
+  {
+    "revision": "657a469469a2fc38a2901c68a3d56512",
+    "url": "/kanji-game/static/media/fa-solid-900.657a4694.woff"
+  },
+  {
+    "revision": "a547e21eceadf53602caf057be9ad9fd",
+    "url": "/kanji-game/static/media/fa-solid-900.a547e21e.eot"
+  },
+  {
+    "revision": "fdc155d57b7351ef85b3028ea3cfc048",
+    "url": "/kanji-game/static/media/fa-solid-900.fdc155d5.svg"
+  },
+  {
+    "revision": "67c7dbb8f097093766798ba3c8a949aa",
+    "url": "/kanji-game/static/media/incorrect.67c7dbb8.wav"
+  }
+]);
